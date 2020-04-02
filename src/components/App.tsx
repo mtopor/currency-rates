0252ccf92a778bd3394951d91c52bbda0afbf606
mt667118 <maksym.topor@broadcom.com>
2020-04-02 12:55:50 +0200

    adding code selector

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,57 +1,92 @@
-import { AgGridReact } from "ag-grid-react";
 import React, { useEffect, useState } from "react";
 
 import DatePicker from "react-datepicker";
+import { AgGridReact } from "ag-grid-react";
+import {ALL_RATES_OPTION} from "../constants/rates-constants";
+
+import { CurrencyData } from "../types/rates";
+import { getTableData } from "../requests/currency-requests";
 
 import "react-datepicker/dist/react-datepicker.css";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
 import "./App.css";
-import { CurrencyData } from "../types/rates";
-import { getTableData } from "../requests/currency-requests";
 
 interface Props {
   text: string;
 }
 
 const columnDefs = [
-    {
-      headerName: "Country",
-      field: "country",
-    },
-    {
-      headerName: "Code",
-      field: "code",
-    },
-    {
-      headerName: "Rate",
-      field: "rate",
-    },
-  ];
+  {
+    headerName: "Country",
+    field: "country",
+  },
+  {
+    headerName: "Code",
+    field: "code",
+  },
+  {
+    headerName: "Rate",
+    field: "rate",
+  },
+];
 
 const App: React.FC<Props> = ({ text }) => {
   const [pickedDate, setPickedDate] = useState(new Date());
+  const [selectedRateCode, setSelectedRateCode] = useState(ALL_RATES_OPTION); //todo all codes by def
+  const [ratesData, setRatesData] = useState<CurrencyData[]>([]);
   const [tableData, setTableData] = useState<CurrencyData[]>([]);
+
+  useEffect(() => {
+    getTableData(pickedDate).then((response) => {
+      setRatesData(response.data); //todo get from servers initial state
+      setTableData(response.data); //todo get from servers initial state
+    });
+  }, []);
 
   useEffect(() => {
     if (pickedDate) {
       getTableData(pickedDate).then((response) => {
-        setTableData(response.data);
+        setRatesData(response.data);
       });
     }
   }, [pickedDate]);
+
+  const handleSelected = (selectedValue: string): void => {
+    setSelectedRateCode(selectedValue);
+    if (selectedValue === ALL_RATES_OPTION) {
+      setTableData(ratesData);
+    } else {
+      setTableData(ratesData.filter((row) => row.code === selectedValue));
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <span>{`Hello from component ${text}`}</span>
-
+        <span>select date</span>
         <DatePicker
           showPopperArrow={false}
           selected={pickedDate}
           dateFormat={"dd.MM.yyyy"}
           onChange={(date: any) => setPickedDate(date)}
         />
+        <span>select currency code</span>
+        <select
+          onChange={(e: any) => {
+            handleSelected(e.target.value);
+          }}
+        >
+          <option value={ALL_RATES_OPTION} key={0}>
+            All rates
+          </option>
+          {ratesData.map((data: any, index: any) => (
+            <option value={data.code} key={index + 1}>
+              {data.code}
+            </option>
+          ))}
+        </select>
 
         <div
           className="ag-theme-balham"
@@ -60,10 +95,7 @@ const App: React.FC<Props> = ({ text }) => {
             width: "600px",
           }}
         >
-          <AgGridReact
-            columnDefs={columnDefs}
-            rowData={tableData}
-          />
+          <AgGridReact columnDefs={columnDefs} rowData={tableData} />
         </div>
       </header>
     </div>
---
+++ src/constants/rates-constants.ts
@@ -0,0 +1,1 @@
+export const ALL_RATES_OPTION = 'All rates';
