fa67ebc4566127d2a3548424120563bff11fb9d2
mt667118 <maksym.topor@broadcom.com>
2020-04-06 20:47:35 +0200

    adding styles

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -37,7 +37,6 @@ const columnDefs = [
   },
 ];
 
-//todo styles
 const App: React.FC<AppProps> = ({ tableData }) => {
   const [state, dispatch] = useReducer(reducer, {
     tableData: tableData,
@@ -67,45 +66,53 @@ const App: React.FC<AppProps> = ({ tableData }) => {
   };
   return (
     <div className="App">
-      <div className="App-header">
-        <span>Uber Rates</span>
-        <div>
-          <span>{`Select date `}</span>
-          <DatePicker
-            showPopperArrow={false}
-            selected={state.selectedDate}
-            dateFormat={'dd.MM.yyyy'}
-            onChange={(date: Date) => handleDatePicked(date)}
-          />
-        </div>
-        <div>
-          <span>{`Select currency `}</span>
-          <SelectComponent
-            onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-              dispatch(setCurrencyCode(e.target.value));
-            }}
-            options={state.tableData?.map((data: CurrencyData) => data.code)}
-            defaultOption={ALL_RATES_OPTION}
-          />
-        </div>
-        <div className="rates-table">
-          {!!state.error.length && <span>{state.error}</span>}
-          {state.isLoading ? (
-            <SpinnerComponent />
-          ) : (
-            !state.error.length && (
-              <BootstrapTable
-                bodyClasses="rates-table"
-                keyField={'code'}
-                data={tableData}
-                columns={columnDefs}
-                striped
-                hover
-                condensed
-                bootstrap4
-              />
-            )
-          )}
+      <div className="App-container">
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4">Uber Rates</h1>
+            <hr />
+
+            <p className="lead">{`Select date `}</p>
+            <DatePicker
+              className="form-control form-control-lg"
+              showPopperArrow={false}
+              selected={state.selectedDate}
+              dateFormat={'dd.MM.yyyy'}
+              onChange={(date: Date) => handleDatePicked(date)}
+            />
+            <br />
+            <br />
+
+            <p className="lead">{`Select currency `}</p>
+            <SelectComponent
+              onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+                dispatch(setCurrencyCode(e.target.value));
+              }}
+              options={state.tableData?.map((data: CurrencyData) => data.code)}
+              defaultOption={ALL_RATES_OPTION}
+            />
+          </div>
+
+          <div className="rates-table align-middle">
+            {!!state.error.length && <span>{state.error}</span>}
+            {state.isLoading ? (
+              <SpinnerComponent />
+            ) : (
+              !state.error.length && (
+                <BootstrapTable
+                  bodyClasses="rates-table"
+                  headerClasses="thead-dark"
+                  keyField={'code'}
+                  data={tableData}
+                  columns={columnDefs}
+                  striped
+                  hover
+                  condensed
+                  bootstrap4
+                />
+              )
+            )}
+          </div>
         </div>
       </div>
     </div>
